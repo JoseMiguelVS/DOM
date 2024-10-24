d0165bdfa6801e1be11ef6e4a5c19673a5b46e28
JossMikee <josemiguelvazquezsanchez682@gmail.com>
2024-10-23 21:53:36 -0600

    Menu en index + tareasApp restaurado

--- colorsinput/app.js
+++ colorsinput/app.js
@@ -2,6 +2,7 @@ const inputRed = document.querySelector('#red');
 const inputBlue = document.querySelector('#blue');
 const inputGreen = document.querySelector('#green');
 const body = document.querySelector('body');
+const span = document.querySelector('h2 span');
 
 let redColor = 0;
 let greenColor = 0;
@@ -14,14 +15,17 @@ const generateColor = () => {
 inputRed.addEventListener('change', (e) => {
     redColor = e.target.value;
     body.style.backgroundColor = generateColor();
+    span.innerText = generateColor();
 });
 
 inputBlue.addEventListener('change', (e) => {
     blueColor = e.target.value;
     body.style.backgroundColor = generateColor();
+    span.innerText = generateColor();
 });
 
 inputGreen.addEventListener('change', (e) => {
     greenColor = e.target.value;
     body.style.backgroundColor = generateColor();
+    span.innerText = generateColor();
 });
--- tareasApp/app.js
+++ tareasApp/app.js
@@ -1,28 +1,78 @@
-const inputTarea = document.getElementById('tareatxt');
+const inputTarea = document.getElementById('tareatext');
 const btnAdd = document.getElementById('addtarea');
 const divTareas = document.getElementById('tareas');
+const myForm = document.querySelector('#myForm');
+
+let arrayTareas = localStorage.getItem('tareas')
+    ? JSON.parse(localStorage.getItem('tareas'))
+    : [];
 
 const addTarea = () => {
     const tarea = inputTarea.value;
-    divTareas.appendChild(createTarea(tarea)); //agregar un hijo, agregar
+    divTareas.appendChild(createTarea(tarea));
     inputTarea.value = '';
+    arrayTareas.push(
+        {
+            tarea: tarea,
+            estado: 'danger',
+            id: Math.floor(Math.random() * 10000)
+        }
+    );
+    construyeDivs();
+    localStorage.setItem('tareas', JSON.stringify(arrayTareas));
 }
 
-const deleteIteam = (e) => {
-    const pa = e.target.parentElement; //accion de borrar
+const construyeDivs = () => {
+    divTareas.innerHTML = '';
+    arrayTareas.forEach(tarea => {
+        divTareas.appendChild(createTarea(tarea))
+    });
+}
+
+
+const deleteitem = (e) => {
+    const pa = e.target.parentElement;
+    const id = pa.getAttribute('data-id');
+    arrayTareas = arrayTareas.filter(tarea => tarea.id !== Number(id));
     pa.remove();
+    localStorage.setItem('tareas', JSON.stringify(arrayTareas));
 }
 
 const createTarea = (tarea) => {
     const div = document.createElement('div');
     const p = document.createElement('p');
     const button = document.createElement('button');
-    button.addEventListener('click', deleteIteam); //se agrega la funcion del boton "eliminar"
-    p.innerText = tarea; //Imprime lo que se ingresa por teclado
-    button.innerText = 'Eliminar'; //agregar texto a un boton
-    div.appendChild(p); //meter algo en otra cosa
-    div.appendChild(button);
+    p.innerText = tarea.tarea;
+    button.innerText = 'Eliminar';
+    button.addEventListener('click', deleteitem)
+    div.appendChild(p);
+    div.append(button);
+    div.classList.add('alert');
+    div.classList.add(tarea.estado);
+    div.setAttribute('data-id', tarea.id);
+    div.addEventListener('click', changeColor)
     return div;
 }
 
-btnAdd.addEventListener('click', addTarea);
+const changeColor = (e) => {
+    const cDiv = e.target;
+    const id = cDiv.getAttribute('data-id');
+    const encontrado = arrayTareas.findIndex(tarea => tarea.id == id);
+    if (cDiv.classList.contains('danger')) {
+        cDiv.classList.remove('danger');
+        cDiv.classList.add('warning');
+        arrayTareas[encontrado].estado = "warning"
+    } else if (cDiv.classList.contains('warning')) {
+        cDiv.classList.remove('warning');
+        cDiv.classList.add('success');
+        arrayTareas[encontrado].estado = "success"
+    }
+    localStorage.setItem('tareas', JSON.stringify(arrayTareas));
+}
+construyeDivs();
+
+//btnAdd.addEventListener('click', addTarea);
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTarea();
+})
